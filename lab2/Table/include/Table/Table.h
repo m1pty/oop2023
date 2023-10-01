7a6fd2605cd15191cd35506167a9c72d7ed5c0d5
m1pty <equals.dvdt@gmail.com>
2023-10-01 21:32:13 +0300

    Updating Table.h

--- lab2/Table/include/Table/Table.h
+++ lab2/Table/include/Table/Table.h
@@ -12,22 +12,65 @@ namespace TNS {
         int csize = 0;
 
     public:
-        Fullness is_full();
         // -создание экземпляров класса с инициализацией заданным количеством ресурсов из массива ресурсов;
-        // -(+=) добавление нового ресурса в таблицу;
-        // -([]) получение ресурса по его наименованию (при помощи двоичного поиска; 
-        // возврат по ссылке);
-        // -проверка состояния таблицы (пустая, частично заполнена или полная);
-        // -удаление ресурса с заданным наименованием из таблицы;
-        // -вычисление общей прибыльности по всем ресурсам;
-        // -(*) увеличение оборота всех ресурсов в заданное число раз;
-        // -переименование заданного ресурса (передаются старое и новое наименования).
+        // конструктор, деструктор
 
-        void add(const RNS::Resource &r);   // добавление ресурса в таблицу
-        int searchByName(std::string name); // поиск по имени
-        RNS::Resource &getByName(std::string name); // после успешного поиска - изъятие по ссылке
-        std::string checkFullness();
+        /*!
+            Returns a state of the table (full / partly full / empty)
+            @returns A state of the table (full / partly full / empty)
+        */
+        Fullness checkFullness();
 
+        /*!
+            Adding the resource to the table by it's link
+            @param r a link to the adding resource
+        */
+        void add(const RNS::Resource &r);   // (+=) добавление ресурса в таблицу
+        
+        /*!
+            Deletes the resource from the table by it's name
+            @param name The name of the deleting resource
+        */
+        void deleteByName(std::string name); // удаляет ресурс из таблицы по наименованию
+
+        /*!
+            Deletes the resource from the table by it's index
+            @param index The index of the deleting resource
+        */
+        void deleteByIndex(int index); // удаляет ресур по индексу
+
+        /*!
+            Returns a pointer to array of <name> resource indices
+            @param name the name of the searched resource 
+            @returns a pointer to array of <name> resource indices
+        */
+        int* searchByName(std::string name); // поиск ресурсов в таблице 
+
+        /*!
+            Returns a link to the new printable table, containing all examples of this result
+            @param indices a pointer to array of integers, containing indecies of resource
+            @returns a link to the new printable table, containing all examples of this result
+        */
+        ResourceTable &searchResult(int* indices); // ([]) получение таблицы найденных ресурсов, при помощи двоичного поиска
+    
+        /*!
+            Changing the name of the resource
+            @param old_name An old name of the resource
+            @param new_name A new name of the resource
+        */
+        void rename(std::string old_name, std::string new_name);
+
+        /*!
+            Increasing the turnover of all resources in <multiplier> times
+            @param multiplier A multuiplier for increasing the turnover of all resources
+        */
+        void incTurnover(double multipliter); // (*) увеличение оборота всех ресурсов
+
+        /*!
+            Returns a profit for all of the resources in summary
+            @returns A profit for all of the resources in summary
+        */
+        double getProfit();
     };
 }
 
